423e1943ee780275f9f9e0ebf323a492b349e3a7
dhavalgondaliya27 <bhavnagondaliya77@gmail.com>
2024-04-10 10:53:49 +0530

    Docker

--- src/app.js
+++ src/app.js
@@ -16,17 +16,17 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000',],
     credentials: true,
   })
 );
-app.use((req, res, next) => {
-  // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Allow requests from any origin
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow specified HTTP methods
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specified headers
-  next();
-});
+// app.use((req, res, next) => {
+//   // Set CORS headers
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Allow requests from any origin
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow specified HTTP methods
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specified headers
+//   next();
+// });
 app.use(
   session({
     secret: 'Dhaval Gondaliya',
