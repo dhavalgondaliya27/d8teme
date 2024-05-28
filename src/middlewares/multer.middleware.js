ae4ad409b940c9e625c379c9c11d38498a017df3
dhavalgondaliya27 <bhavnagondaliya77@gmail.com>
2024-05-28 17:18:10 +0530

    Done

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,10 +1,10 @@
-import multer from "multer";
+import multer from 'multer';
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    cb(null, './public/temp');
   },
   filename: function (req, file, cb) {
-    cb(null, `./public/temp/${file.originalname}`);
+    cb(null, file.originalname);
   },
 });
 export const upload = multer({ storage });
